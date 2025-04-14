3ad44d2f1ff84e05c2c9bc262f97739eb3755ac4
Sorin Raduta <sorinradutaa@gmail.com>
2025-04-14 22:31:06 +0300

    Clock fix

--- src/components/ClockModel.tsx
+++ src/components/ClockModel.tsx
@@ -1,15 +1,18 @@
 import { useGLTF } from "@react-three/drei";
+import { useEffect } from "react";
 
 export default function ClockModel() {
   const { scene } = useGLTF("/assets/models/clock.glb");
 
-  const clockHourPin = scene.getObjectByName("mdlwc_2_hour_pointer");
-  clockHourPin?.position.set(4.9, 69.2, 0.7);
-  clockHourPin?.rotateY(1.45);
+  useEffect(() => {
+    const clockHourPin = scene.getObjectByName("mdlwc_2_hour_pointer");
+    clockHourPin?.position.set(4.9, 69.2, 0.7);
+    clockHourPin?.rotateY(1.45);
 
-  const clockMinutePin = scene.getObjectByName("mdlwc_2_min_pointer");
-  clockMinutePin?.position.set(-8.85, 49.3, 1);
-  clockMinutePin?.rotateY(2.88);
+    const clockMinutePin = scene.getObjectByName("mdlwc_2_min_pointer");
+    clockMinutePin?.position.set(-8.85, 49.3, 1);
+    clockMinutePin?.rotateY(2.88);
+  }, [scene]);
 
   scene.scale.set(0.4, 0.4, 0.4);
   scene.position.set(2.414, 1.5, -3.7);
